39aa6f928146e5815b447b34ddfd61e2de9f4ddb
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-16 14:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeColor() {
+      const select = document.getElementById("colorSelect");
+      const selectedOption = select.value;
+      const optionIndex = select.selectedIndex;
+      select.remove(optionIndex);
+    }
